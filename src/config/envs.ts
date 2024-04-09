3a37a69168cd755a78390bbd2df341151cb47dc9
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-09 12:20:12 -0600

    feat: connect products-ms with gateway

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,11 +5,14 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
-    DATABASE_URL: string;
+    PRODUCTS_MICROSERVICE_HOST: string
+    PRODUCTS_MICROSERVICE_PORT: number
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
 }).unknown();
 
 const {error, value} = envsSchema.validate(process.env);
@@ -20,5 +23,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
-    databaseUrl: envVars.DATABASE_URL
+    productMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+    productMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
 }
